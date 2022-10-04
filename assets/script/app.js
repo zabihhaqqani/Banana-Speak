3c63db6bc21da5a265929e20b7cd0e11c94828ab
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-10-04 21:19:13 +0530

    Added light and  dark mode toggle button

--- assets/script/app.js
+++ assets/script/app.js
@@ -38,3 +38,10 @@ function clickHandler() {
 }
 
 btnTranslate.addEventListener("click", clickHandler);
+
+// select toggle button
+let toggle = document.getElementById("mode");
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+})
